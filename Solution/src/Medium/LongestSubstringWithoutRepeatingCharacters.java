01a1860bafd1d4191765eb7c3d0fcc31a2ed8658
161158053 <30664307+Cauchy-NY@users.noreply.github.com>
2018-09-04 21:00:21 +0800

    add a solution and a problem

---
+++ Solution/src/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+package Medium;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+}
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}

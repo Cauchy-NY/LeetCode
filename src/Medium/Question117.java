d66afd3d2761d3b1648378d982a1fc5972809498
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2018-11-09 11:27:08 +0800

    add

---
+++ src/Medium/Question117.java
@@ -0,0 +1,21 @@
+package Medium;
+
+import util.TreeLinkNode;
+
+public class Question117 {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode levelStart = root;
+        while(levelStart != null) {
+            TreeLinkNode cur = levelStart;
+            while(cur != null) {
+                if(cur.left != null)
+                    cur.left.next = cur.right;
+                if(cur.right != null && cur.next != null)
+                    cur.right.next = cur.next.left;
+
+                cur=cur.next;
+            }
+            levelStart = levelStart.left;
+        }
+    }
+}

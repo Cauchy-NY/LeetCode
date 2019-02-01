7c585d34a401887e1db63cb5c4701280ce90244a
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-01 11:45:03 +0800

    长的像图的深拷贝果然要记录visit

---
+++ src/main/java/Medium/Question138.java
@@ -0,0 +1,29 @@
+package Medium;
+
+import util.RandomListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question138 {
+
+    private Map<RandomListNode, RandomListNode> visitedMap = new HashMap<>();
+
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null)
+            return null;
+
+        if (visitedMap.containsKey(head)) {
+            return visitedMap.get(head);
+        }
+
+        RandomListNode newNode = new RandomListNode(head.label);
+
+        visitedMap.put(head, newNode);
+
+        newNode.next = copyRandomList(head.next);
+        newNode.random = copyRandomList(head.random);
+
+        return newNode;
+    }
+}

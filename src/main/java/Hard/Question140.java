6646bdb9dc190cbf0f9251e32729f84b22be2f36
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-08 10:43:35 +0800

    评论区大佬的答案，还是带记忆的dfs，但是不能直接由上一题改，上一题记录的是不可能的串，因为要记录可能的串的排列，所以要记录可能的串

---
+++ src/main/java/Hard/Question140.java
@@ -0,0 +1,39 @@
+package Hard;
+
+import java.util.*;
+
+public class Question140 {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return DFS(s, wordDict, new HashMap<>());
+    }
+
+    private List<String> DFS(String s, List<String> wordDict, HashMap<String, LinkedList<String>>map) {
+        if (map.containsKey(s))
+            return map.get(s);
+
+        LinkedList<String> res = new LinkedList<>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String> sublist = DFS(s.substring(word.length()), wordDict, map);
+                for (String sub: sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+            }
+        }
+        map.put(s, res);
+        return res;
+    }
+
+    public static void  main(String[] args) {
+        List<String> wordDict = new LinkedList<>();
+        wordDict.add("apple");
+        wordDict.add("pen");
+        wordDict.add("applepen");
+        wordDict.add("pine");
+        wordDict.add("pineapple");
+        new Question140().wordBreak("pineapplepenapple", wordDict);
+    }
+}

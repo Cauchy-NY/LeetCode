4c37f03331b491ec7d92e3732fdef519b6271678
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-13 11:21:44 +0800

    又是一个快慢指针的题目

---
+++ src/main/java/Medium/Question143.java
@@ -0,0 +1,54 @@
+package Medium;
+
+import util.ListNode;
+
+public class Question143 {
+    public void reorderList(ListNode head) {
+        if (head == null)
+            return;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode cur1 = head;
+        ListNode cur2 = reverse(slow.next);
+        slow.next = null;
+
+        while (cur1 != null && cur2 != null) {
+            ListNode tmpCur1 = cur1;
+            ListNode tmpCur2 = cur2;
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+            tmpCur1.next = tmpCur2;
+            tmpCur2.next = cur1;
+        }
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode p1 = null;
+        ListNode p2 = head;
+        ListNode p3;
+
+        while (p2 != null) {
+            p3 = p2.next;
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+
+        return p1;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = ListNode.getLinkedList(4);
+        new Question143().reorderList(head);
+        System.out.println(ListNode.toString(head));
+    }
+}
--- src/main/java/util/ListNode.java
+++ src/main/java/util/ListNode.java
@@ -8,4 +8,32 @@ public class ListNode {
         val = x;
         next = null;
     }
+
+    public static ListNode getLinkedList(int len) {
+        if (len < 1)
+            return null;
+
+        ListNode head = new ListNode(1);
+        ListNode cur = head;
+        for (int i = 1; i < len; i++) {
+            ListNode node = new ListNode(i+1);
+            cur.next = node;
+            cur = node;
+        }
+
+        return head;
+    }
+
+    public static String toString(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        while (head != null) {
+            sb.append(head.val);
+            if (head.next != null)
+                sb.append("->");
+            head = head.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }

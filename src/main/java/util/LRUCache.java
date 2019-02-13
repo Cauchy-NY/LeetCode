990621c4798359ddefdfe492ef5796dde27c1e63
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-13 14:37:54 +0800

    好像也是道很经典的题目，如果用linkedlist不自己写双链表的话，时间复杂度就到不了O(1)

---
+++ src/main/java/util/LRUCache.java
@@ -0,0 +1,61 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private int capacity;
+    private Node head = new Node(0, 0);
+    private Node tail = new Node(0, 0);
+    private Map<Integer, Node> map = new HashMap<>();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else
+            return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key))
+            remove(map.get(key));
+        if (capacity == map.size())
+            remove(tail.prev);
+        insert(new Node(key, value));
+    }
+
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node = null;
+    }
+
+    private void insert(Node node){
+        map.put(node.key, node);
+        Node headNext = head.next;
+        head.next = node;
+        node.prev = head;
+        headNext.prev = node;
+        node.next = headNext;
+    }
+}

73738bd8245db7f0bdafb62203d6b82b4a92dc77
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-14 12:33:09 +0800

    链表的插入排序

---
+++ src/main/java/Medium/Question147.java
@@ -0,0 +1,35 @@
+package Medium;
+
+import util.ListNode;
+
+public class Question147 {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode newHead = new ListNode(Integer.MIN_VALUE);
+        ListNode newTail = new ListNode(Integer.MAX_VALUE);
+        newHead.next = newTail;
+
+        while (head != null) {
+            ListNode cur = head;
+            head = head.next;
+
+            ListNode beforeTmpCur = newHead;
+            ListNode tmpCur = newHead;
+            while (tmpCur != newTail) {
+                tmpCur = tmpCur.next;
+                if (cur.val <= tmpCur.val) {
+                    beforeTmpCur.next = cur;
+                    cur.next = tmpCur;
+                    break;
+                }
+                beforeTmpCur = beforeTmpCur.next;
+            }
+        }
+
+        ListNode cur = newHead;
+        while (cur.next != newTail)
+            cur = cur.next;
+        cur.next = null;
+
+        return newHead.next;
+    }
+}

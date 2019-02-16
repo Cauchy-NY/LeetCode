ea2464cf9b063ff3daf8ae29ae9b815c7e5b6b2f
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-16 11:14:45 +0800

    花式二分查找

---
+++ src/main/java/Medium/Question153.java
@@ -0,0 +1,16 @@
+package Medium;
+
+public class Question153 {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length-1;
+        while (left < right) {
+            int mid = left + ((right-left) >> 1);
+
+            if (nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return nums[left];
+    }
+}

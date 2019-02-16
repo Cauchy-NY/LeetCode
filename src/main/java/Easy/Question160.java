9447e07149a674006b9cc2b976a57cc3b0e3c1fd
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-16 13:20:41 +0800

    又被dalao的答案秀到了，不用hash，每次交换头长短距离-1，直到相遇，不想交也会最终交于null

---
+++ src/main/java/Easy/Question160.java
@@ -0,0 +1,46 @@
+package Easy;
+
+import util.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //boundary check
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        //if a & b have different len, then we will stop the loop after second iteration
+        while (a != b) {
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            a = a == null? headB : a.next;
+            b = b == null? headA : b.next;
+        }
+
+        return a;
+    }
+
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if (headA == null || headB == null)
+//            return null;
+//
+//        Set<ListNode> set = new HashSet<>();
+//        ListNode cur = headA;
+//        while (cur != null) {
+//            set.add(cur);
+//            cur = cur.next;
+//        }
+//
+//        cur = headB;
+//        while (cur != null) {
+//            if (set.contains(cur))
+//                return cur;
+//            cur = cur.next;
+//        }
+//        return null;
+//    }
+}

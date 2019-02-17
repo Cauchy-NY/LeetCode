310609217784666be3b75751e63c1525b0f45c6d
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-17 13:29:47 +0800

    和26进制还是有点区别的

---
+++ src/main/java/Easy/Question168.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class Question168 {
+    public String convertToTitle(int n) {
+        StringBuilder res = new StringBuilder();
+        while (n-- != 0) {
+            res.insert(0, (char)(n%26 + 'A'));
+            n /= 26;
+        }
+        return res.toString();
+    }
+}

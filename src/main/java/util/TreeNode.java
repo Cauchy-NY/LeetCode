90051568e610a2af2e84dc0ba5ce1709e5f640fa
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-17 19:13:54 +0800

    add 由数组构建二叉树方法

--- src/main/java/util/TreeNode.java
+++ src/main/java/util/TreeNode.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 // Definition for a binary tree node.
 public class TreeNode {
     public int val;
@@ -8,5 +11,55 @@ public class TreeNode {
 
     public TreeNode(int x) {
         val = x;
+    }
+
+    /**
+     * eg: [5,4,8,11,null,13,4,7,2,null,null,null,1] 构建的树的结构如下：
+     *       5
+     *      / \
+     *     4   8
+     *    /   / \
+     *   11  13  4
+     *  /  \      \
+     * 7    2      1
+     * @param nums 构件树的数组
+     * @return 按要求构建出的树的根节点
+     */
+    public static TreeNode constructBinaryTree(Integer[] nums){
+        if (nums.length == 0) return new TreeNode(0);
+        Deque<TreeNode> nodeQueue = new LinkedList<>();
+        // 创建一个根节点
+        TreeNode root = new TreeNode(nums[0]);
+        nodeQueue.offer(root);
+        TreeNode cur;
+        // 记录当前行节点的数量（注意不一定是2的幂，而是上一行中非空节点的数量乘2）
+        int lineNodeNum = 2;
+        // 记录当前行中数字在数组中的开始位置
+        int startIndex = 1;
+        // 记录数组中剩余的元素的数量
+        int restLength = nums.length - 1;
+
+        while(restLength > 0) {
+            // 只有最后一行可以不满，其余行必须是满的
+            for (int i = startIndex; i < startIndex + lineNodeNum; i = i + 2) {
+                // 说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i == nums.length) return root;
+                cur = nodeQueue.poll();
+                if (nums[i] != null) {
+                    cur.left = new TreeNode(nums[i]);
+                    nodeQueue.offer(cur.left);
+                }
+                // 同上，说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i + 1 == nums.length) return root;
+                if (nums[i + 1] != null) {
+                    cur.right = new TreeNode(nums[i + 1]);
+                    nodeQueue.offer(cur.right);
+                }
+            }
+            startIndex += lineNodeNum;
+            restLength -= lineNodeNum;
+            lineNodeNum = nodeQueue.size() * 2;
+        }
+        return root;
     }
 }

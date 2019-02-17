4f34971658830e5cb71deef35156c9ae9537ea68
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-17 19:14:18 +0800

    二叉搜索树的中序遍历

---
+++ src/main/java/Medium/Question173.java
@@ -0,0 +1,23 @@
+package Medium;
+
+import util.BSTIterator;
+import util.TreeNode;
+
+public class Question173 {
+
+    public static void main(String[] args) {
+        Integer[] treeNums = {5,4,8,11,null,13,4,7,2,null,null,null,1};
+        TreeNode root = TreeNode.constructBinaryTree(treeNums);
+
+        BSTIterator iterator = new BSTIterator(root);
+        iterator.next();    // 返回 3
+        iterator.next();    // 返回 7
+        iterator.hasNext(); // 返回 true
+        iterator.next();    // 返回 9
+        iterator.hasNext(); // 返回 true
+        iterator.next();    // 返回 15
+        iterator.hasNext(); // 返回 true
+        iterator.next();    // 返回 20
+        iterator.hasNext(); // 返回 false
+    }
+}
---
+++ src/main/java/util/BSTIterator.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+// 非递归写法
+public class BSTIterator {
+
+    private TreeNode cur;
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        cur = root;
+        stack = new Stack<>();
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        cur = stack.pop();
+        int res = cur.val;
+        cur = cur.right;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        return res;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+//// 递归写法
+//public class BSTIterator {
+//
+//    private Queue<Integer> queue = new LinkedList<>();
+//
+//    public BSTIterator(TreeNode root) {
+//        inOrder(root);
+//    }
+//
+//    /** @return whether we have a next smallest number */
+//    public boolean hasNext() {
+//        return !queue.isEmpty();
+//    }
+//
+//    /** @return the next smallest number */
+//    public int next() {
+//        return queue.poll();
+//    }
+//
+//    private void inOrder(TreeNode root){
+//        if(root == null)
+//            return;
+//        inOrder(root.left);
+//        queue.add(root.val);
+//        inOrder(root.right);
+//    }
+//}

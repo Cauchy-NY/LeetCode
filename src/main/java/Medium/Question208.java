23c87c0223758e30b8f33fa1d5608af49bf7c86a
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-21 18:30:10 +0800

    前缀树

---
+++ src/main/java/Medium/Question208.java
@@ -0,0 +1,16 @@
+package Medium;
+
+import util.Trie;
+
+public class Question208 {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        trie.search("apple");   // 返回 true
+        trie.search("app");     // 返回 false
+        trie.startsWith("app"); // 返回 true
+        trie.insert("app");
+        trie.search("app");     // 返回 true
+    }
+}

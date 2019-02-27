4ec2ac74f2cfebd6d2db825790328296e1366f91
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-27 11:56:03 +0800

    有名的队列实现栈，不需要双队列，把之前的值add到后面就好了

---
+++ src/main/java/Easy/Question225.java
@@ -0,0 +1,15 @@
+package Easy;
+
+import util.MyStack;
+
+public class Question225 {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.top();   // returns 2
+        stack.pop();   // returns 2
+        stack.empty(); // returns false
+    }
+}

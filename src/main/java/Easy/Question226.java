1143c80a9261b9f6faff6760a79f182a71b27f95
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-02-28 10:46:36 +0800

    翻转二叉树

---
+++ src/main/java/Easy/Question226.java
@@ -0,0 +1,46 @@
+package Easy;
+
+import util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Question226 {
+    // bfs
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+
+            if (node.left != null)
+                queue.offer(node.left);
+
+            if (node.right != null)
+                queue.offer(node.right);
+        }
+        return root;
+    }
+
+    //dfs
+    public TreeNode invertTree_2(TreeNode root) {
+        if (root == null)
+            return null;
+
+        invertTree_2(root.left);
+        invertTree_2(root.right);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        return root;
+    }
+}

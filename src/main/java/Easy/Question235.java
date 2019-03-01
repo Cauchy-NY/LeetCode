0510efafd7e7fa3e9877d233df35257a3c74555e
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-01 13:26:03 +0800

    最近公共最先，也是很有名的一道题

---
+++ src/main/java/Easy/Question235.java
@@ -0,0 +1,25 @@
+package Easy;
+
+import util.TreeNode;
+
+public class Question235 {
+    // 递归
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null)
+            return null;
+
+        if ((root.val - p.val) * (root.val - q.val) <= 0)
+            return root;
+        else if (root.val - p.val < 0)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return lowestCommonAncestor(root.left, p, q);
+    }
+
+    // 迭代
+    public TreeNode lowestCommonAncestor_2(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+    }
+}

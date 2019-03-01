543d3acd10f523115bc53bc43b6a5f35828c9dfe
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-01 12:40:07 +0800

    这方法应该是栈模拟队列里面最优的了，尽可能的将倒来倒去的操作减至最少

---
+++ src/main/java/Easy/Question232.java
@@ -0,0 +1,15 @@
+package Easy;
+
+import util.MyQueue;
+
+public class Question232 {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+
+        queue.push(1);
+        queue.push(2);
+        queue.peek();  // 返回 1
+        queue.pop();   // 返回 1
+        queue.empty(); // 返回 false
+    }
+}

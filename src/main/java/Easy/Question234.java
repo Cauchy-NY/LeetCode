a426170ef4ba7381c9736c17f5a7fa94ee150caa
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-01 13:13:11 +0800

    第一个想到的是双指针找中点+栈，但评论区方法—双指针找中点切分两条链要稍微快一点，空间消耗也小

---
+++ src/main/java/Easy/Question234.java
@@ -0,0 +1,59 @@
+package Easy;
+
+import util.ListNode;
+
+import java.util.Stack;
+
+public class Question234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome_2(ListNode head) {
+        ListNode slow = head, fast = head;
+        Stack<Integer> stack = new Stack<>();
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null)
+            slow = slow.next;
+
+        while (slow != null) {
+            if (stack.pop() != slow.val)
+                return false;
+            slow = slow.next;
+        }
+        return true;
+    }
+}

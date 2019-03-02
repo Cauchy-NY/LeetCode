2cf405c779e795bbae17b56ef0cc314c366de135
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-02 12:56:35 +0800

    这道题是个笑话

---
+++ src/main/java/Easy/Question237.java
@@ -0,0 +1,10 @@
+package Easy;
+
+import util.ListNode;
+
+public class Question237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}

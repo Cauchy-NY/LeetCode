cc06621ab63646cf9b598f515eb2b2dab9675c9b
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-05 12:28:02 +0800

    设计一个可以peek的迭代器

---
+++ src/main/java/Medium/Question284.java
@@ -0,0 +1,22 @@
+package Medium;
+
+import util.PeekingIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question284 {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+
+        PeekingIterator peekingIterator = new PeekingIterator(integerList.iterator());
+        peekingIterator.next();
+        peekingIterator.peek();
+        peekingIterator.next();
+        peekingIterator.next();
+        peekingIterator.hasNext();
+    }
+}
---
+++ src/main/java/util/PeekingIterator.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.util.Iterator;
+
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+public class PeekingIterator implements Iterator<Integer> {
+
+    private Iterator<Integer> iterator;
+    private Integer integer;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.iterator = iterator;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if (integer == null && iterator.hasNext())
+            integer = iterator.next();
+        return integer;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if (integer != null) {
+            Integer res = integer;
+            integer = null;
+            return res;
+        } else
+            return iterator.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return integer != null || iterator.hasNext();
+    }
+}

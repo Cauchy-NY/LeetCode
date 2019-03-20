4593867378314de140990ac414d3aa1601b0e1ad
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-03-20 10:57:48 +0800

    神奇的踩气球，我已经连这道dp的答案都看不懂了，我只能想出n!的方案，菜的抠脚

--- src/main/java/util/Test.java
+++ src/main/java/util/Test.java
@@ -1,16 +1,26 @@
 package util;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.Collections.*;
 
 public class Test implements Cloneable {
 
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("-?\\d+");
-        Matcher m = p.matcher("n23");
-        System.out.println(m.matches());
-
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb.toString());
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        int idx = in.nextInt();
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i = 0; i < str.length(); i++) {
+            for(int j = i+1; j <= str.length(); j++) {
+                int tmp = Integer.parseInt(str.substring(i, j));
+                queue.add(tmp);
+                if (queue.size() > idx)
+                    queue.poll();
+            }
+        }
+        System.out.println(queue.poll());
     }
 }

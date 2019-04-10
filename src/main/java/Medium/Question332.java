b3d1721287078ab86b33191b49aa7e5eb997248d
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-04-10 10:08:25 +0800

    这题其实不需要回溯，每次只选字典序最小的机场即可，问题是图的dfs不是很熟练

---
+++ src/main/java/Medium/Question332.java
@@ -0,0 +1,20 @@
+package Medium;
+
+import java.util.*;
+
+public class Question332 {
+    public List<String> findItinerary(String[][] tickets) {
+        HashMap<String, PriorityQueue<String>> targets = new HashMap<>();
+        for (String[] ticket : tickets)
+            targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+        List<String> route = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+        while (!stack.empty()) {
+            while (targets.containsKey(stack.peek()) && !targets.get(stack.peek()).isEmpty())
+                stack.push(targets.get(stack.peek()).poll());
+            route.add(0, stack.pop());
+        }
+        return route;
+    }
+}

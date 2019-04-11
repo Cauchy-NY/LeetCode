92672c47ab20b38f68b3b6adbfc20b322b87cab7
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-04-11 19:45:35 +0800

    扁平化的迭代器，灵活运用栈的例子

---
+++ src/main/java/Medium/Question341.java
@@ -0,0 +1,10 @@
+package Medium;
+
+import Design.NestedIterator;
+
+public class Question341 {
+    public static void main(String[] args) {
+        // 不想写NestedInteger的实现类，所以看不到执行效果
+        NestedIterator nestedIterator;
+    }
+}
